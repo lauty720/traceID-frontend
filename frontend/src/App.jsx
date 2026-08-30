2a63f8a302d7d2a295a76ba8c736a17b2ab54268
lauty720 <lautycall720@gmail.com>
2026-08-30 16:40:00 -0300

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import DonationBar from './components/DonationBar'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -14,6 +15,7 @@ import Admin from './pages/Admin'
 export default function App() {
   return (
     <>
+      <DonationBar />
       <Navbar />
       <main className="page">
         <Routes>
